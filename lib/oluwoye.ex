1254e486eaaa8d85f7df38ab1e21e9d8aafb1807
Vitor Roque <vitor.roquep@gmail.com>
2023-02-27 09:28:07 -0300

    fix: now using elixir

---
+++ lib/oluwoye.ex
@@ -0,0 +1,18 @@
+defmodule Oluwoye do
+  @moduledoc """
+  Documentation for `Oluwoye`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Oluwoye.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/oluwoye_test.exs
@@ -0,0 +1,8 @@
+defmodule OluwoyeTest do
+  use ExUnit.Case
+  doctest Oluwoye
+
+  test "greets the world" do
+    assert Oluwoye.hello() == :world
+  end
+end
