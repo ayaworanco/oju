df1c5ca33aa01793c94e1b6bd4e8654516924c8a
Vitor Roque <vitor.roquep@gmail.com>
2023-03-05 14:38:53 -0300

    feat: adding coverage and config mapper

--- lib/oluwoye/config_mapper.ex
+++ lib/oluwoye/config_mapper.ex
@@ -1,11 +1,42 @@
 defmodule Oluwoye.ConfigMapper do
-  def get_config_from_file(path \\ "./applications.yaml") do
+  def from_file(path \\ "./applications.yaml")
+  def from_file(""), do: %Oluwoye.Exceptions.ConfigMapperError{msg: "No file path passed"}
+  def from_file(nil), do: %Oluwoye.Exceptions.ConfigMapperError{msg: "No file path passed"}
+
+  def from_file(path), do: validate_schema(path)
+
+  defp validate_schema(path) do
     case YamlElixir.read_from_file(path) do
       {:ok, yaml} ->
         yaml
 
       {:error, reason} ->
         %Oluwoye.Exceptions.ConfigMapperError{msg: reason}
+    end
+    |> case do
+      %{"applications" => list_of_apps} when list_of_apps != [] ->
+        has_valid_apps?(list_of_apps)
+
+      %{} ->
+        %Oluwoye.Exceptions.ConfigMapperError{msg: "YAML is empty"}
+    end
+  end
+
+  defp has_valid_apps?(nil),
+    do: %Oluwoye.Exceptions.ConfigMapperError{msg: "No applications found"}
+
+  defp has_valid_apps?(applications) do
+    Enum.map(applications, fn
+      app ->
+        case app do
+          %{"name" => _name, "key" => _key} -> true
+          _ -> false
+        end
+    end)
+    |> Enum.member?(false)
+    |> case do
+      true -> %Oluwoye.Exceptions.ConfigMapperError{msg: "Applications not valid"}
+      false -> applications
     end
   end
 end
--- test/oluwoye/config_mapper_test.exs
+++ test/oluwoye/config_mapper_test.exs
@@ -1,10 +1,52 @@
 defmodule Oluwoye.Test.ConfigMapperTest do
   use ExUnit.Case
 
-  test "should load applications.yaml" do
-    config = Oluwoye.ConfigMapper.get_config_from_file("test/fixtures/applications.yaml")
+  describe "When success config mapping" do
+    test "should load applications.yaml" do
+      config = Oluwoye.ConfigMapper.from_file("test/fixtures/applications.yaml")
 
-    assert %{"applications" => [%{"name" => "worker_php"}, %{"name" => "ruby_on_trails"}]} =
-             config
+      assert [%{"name" => "worker_php"}, %{"name" => "ruby_on_trails"}] = config
+    end
+  end
+
+  describe "When erroring Config Mapper" do
+    test "should return error on nil" do
+      config = Oluwoye.ConfigMapper.from_file(nil)
+
+      assert %Oluwoye.Exceptions.ConfigMapperError{msg: msg} = config
+      assert "No file path passed" = msg
+    end
+
+    test "should return error on empty string" do
+      config = Oluwoye.ConfigMapper.from_file("")
+
+      assert %Oluwoye.Exceptions.ConfigMapperError{msg: msg} = config
+      assert "No file path passed" = msg
+    end
+
+    test "should return error if is a wrong schema" do
+      config = Oluwoye.ConfigMapper.from_file("test/fixtures/error_applications.yaml")
+
+      assert %Oluwoye.Exceptions.ConfigMapperError{msg: msg} = config
+      assert "Applications not valid" = msg
+    end
+
+    test "should not load applications by default (because does not have applications file in root)" do
+      config = Oluwoye.ConfigMapper.from_file()
+      assert %Oluwoye.Exceptions.ConfigMapperError{msg: msg} = config
+      assert "YAML is empty" = msg
+    end
+
+    test "should have not valid apps" do
+      config = Oluwoye.ConfigMapper.from_file("test/fixtures/no_applications.yaml")
+      assert %Oluwoye.Exceptions.ConfigMapperError{msg: msg} = config
+      assert "YAML is empty" = msg
+    end
+
+    test "should have not valid apps by nil" do
+      config = Oluwoye.ConfigMapper.from_file("test/fixtures/nil_applications.yaml")
+      assert %Oluwoye.Exceptions.ConfigMapperError{msg: msg} = config
+      assert "No applications found" = msg
+    end
   end
 end
