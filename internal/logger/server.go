a8a945e94855e6c04b957097d267a4733eba3e48
Vitor Roque <vitor.roquep@gmail.com>
2023-04-17 00:45:46 -0300

    fix: add loading rules

--- internal/logger/server.go
+++ internal/logger/server.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"oluwoye/internal/parser"
+	"oluwoye/internal/ruler"
 	"os"
 )
 
@@ -13,6 +14,15 @@ func StartLogger() {
 	listener, listener_error := net.Listen("tcp", ":"+os.Getenv("PORT"))
 	if listener_error != nil {
 		panic("Logger error: " + listener_error.Error())
+	}
+	rules_file, load_rules_file := load_rules()
+	if load_rules_file != nil {
+		panic("Error loading rule file")
+	}
+
+	rules, load_rules_error := ruler.LoadRules(rules_file)
+	if load_rules_error != nil {
+		panic("Error loading rules")
 	}
 
 	defer listener.Close()
@@ -24,11 +34,20 @@ func StartLogger() {
 			log.Println("Socket Error: ", socket_error.Error())
 		}
 
-		go handle_socket(socket)
+		go handle_socket(socket, rules)
 	}
 }
 
-func handle_socket(socket net.Conn) {
+func load_rules() ([]byte, error) {
+	file, read_error := os.ReadFile(os.Getenv("RULES_YAML_PATH"))
+	if read_error != nil {
+		return nil, read_error
+	}
+
+	return file, nil
+}
+
+func handle_socket(socket net.Conn, rules []ruler.Rule) {
 	log.Println("New connection accepted: ", socket.RemoteAddr().String())
 	reader := bufio.NewReader(socket)
 	for {
@@ -49,6 +68,9 @@ func handle_socket(socket net.Conn) {
 			log.Println("Error on parsing log: ", log_message_error.Error())
 			break
 		}
-		log.Println(log_message)
+
+		for _, rule := range rules {
+			rule.Run(log_message.Message)
+		}
 	}
 }
